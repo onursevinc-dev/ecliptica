e625f688bfd962e694743b6b963822abf478e348
Onur Sevinç <onursevinc.dev@gmail.com>
2024-12-29 01:42:24 +0300

    firefly created, projects page created

--- src/components/StarBackground.jsx
+++ src/components/StarBackground.jsx
@@ -5,9 +5,11 @@ import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
 
-const StarBackground = (props) => {
+export const StarBackground = (props) => {
   const ref = useRef();
-  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
+  const [sphere] = useState(() =>
+    random.inSphere(new Float32Array(5000), { radius: 1.2 })
+  );
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
@@ -17,7 +19,13 @@ const StarBackground = (props) => {
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
       <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
-        <PointMaterial transparent color="#fff" size={0.004} sizeAttenuation={true} dethWrite={false} />
+        <PointMaterial
+          transparent
+          color="#fff"
+          size={0.004}
+          sizeAttenuation={true}
+          dethWrite={false}
+        />
       </Points>
     </group>
   );
