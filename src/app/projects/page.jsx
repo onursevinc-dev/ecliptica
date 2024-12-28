777bcaeb4a94e72e522bb345965cc50de1420d5a
Onur Sevinç <onursevinc.dev@gmail.com>
2024-12-29 01:42:37 +0300

    firefly created, projects page created

---
+++ src/app/projects/page.jsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import bg from "../../../public/background/projects-background.png";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between relative">
+      <Image
+        src={bg}
+        alt="background-image"
+        fill
+        className="-z-50 w-full h-full object-cover object-center opacity-30"
+      />
+      <div className="w-full h-screen"></div>
+    </main>
+  );
+}
